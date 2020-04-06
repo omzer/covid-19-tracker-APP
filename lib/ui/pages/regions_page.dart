9f77991b448980bb99fcecc53664c93b87a4494c
Omar Sabri <oz9.sa7@gmail.com>
2020-04-07 00:00:41 +0300

    Show regions in the regions selection page in a grid

--- lib/ui/pages/regions_page.dart
+++ lib/ui/pages/regions_page.dart
@@ -1,12 +1,24 @@
+import 'package:auto_animated/auto_animated.dart' as animatedLists;
 import 'package:covidtracker/models/region_info.dart';
 import 'package:covidtracker/repo/api.dart';
 import 'package:covidtracker/ui/widgets/world_loading.dart';
+import 'package:covidtracker/utils/assets_utils.dart';
+import 'package:covidtracker/utils/decoration_utils.dart';
 import 'package:covidtracker/utils/navigation_utils.dart';
 import 'package:flutter/material.dart';
 
 class RegionsPage extends StatelessWidget {
   BuildContext _context;
   String _result = 'PS';
+  final scrollController = ScrollController();
+  final options = animatedLists.LiveOptions(
+    showItemInterval: Duration(milliseconds: 50),
+    // Animation duration (default 250)
+    showItemDuration: Duration(milliseconds: 250),
+
+    visibleFraction: 0.25,
+    reAnimateOnVisibility: false,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +27,7 @@ class RegionsPage extends StatelessWidget {
       onWillPop: _onPop,
       child: SafeArea(
         child: Scaffold(
+          appBar: AppBar(elevation: 0, title: Text('Select region:')),
           body: _buildRegionsList(),
         ),
       ),
@@ -34,7 +47,54 @@ class RegionsPage extends StatelessWidget {
   }
 
   Widget _buildRegionsGrid(List<RegionInfo> list) {
-    return Text(list[0].name);
+    return animatedLists.LiveGrid.options(
+      options: options,
+      itemBuilder: (context, index, animation) =>
+          _itemBuilder(animation, list[index]),
+      scrollDirection: Axis.vertical,
+      itemCount: list.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+    );
+  }
+
+  Widget _itemBuilder(animation, region) {
+    return FadeTransition(
+      opacity: Tween<double>(
+        begin: 0,
+        end: 1,
+      ).animate(animation),
+      child: SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(-0.1, 0),
+          end: Offset.zero,
+        ).animate(animation),
+        child: _buildRegionCard(region),
+      ),
+    );
+  }
+
+  Widget _buildRegionCard(RegionInfo region) {
+    final double clipValue = 8;
+    final Widget image = Image.asset(
+      AssetsUtils.getJPGImagePath(region.name),
+      fit: BoxFit.cover,
+    );
+
+    return Card(
+      shape: DecorationUtils.getCardRoundedBorder(clipValue),
+      elevation: 4,
+      child: Column(
+        children: <Widget>[
+          DecorationUtils.clipTopWithRadius(image, clipValue),
+          Text(region.name),
+          Text('cases: ${region.cases}'),
+        ],
+      ),
+    );
   }
 
   Future<bool> _onPop() async {
---
+++ lib/utils/decoration_utils.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DecorationUtils {
+  static ShapeBorder getCardRoundedBorder(double radius) {
+    return RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius));
+  }
+
+  static Widget clipTopWithRadius(Widget widget, double radius) {
+    final Radius radiusValue = Radius.circular(radius);
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topRight: radiusValue,
+        topLeft: radiusValue,
+      ),
+      child: widget,
+    );
+  }
+}
