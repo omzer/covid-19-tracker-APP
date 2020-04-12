9fe5e4c870333993aef0c9725d9b87661c449da0
Omar Sabri <oz9.sa7@gmail.com>
2020-04-13 02:10:08 +0300

    Add support for shared prefs in the app to cache some date

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
+import 'package:covidtracker/db/prefs.dart';
 import 'package:covidtracker/ui/pages/home_page.dart';
+import 'package:covidtracker/ui/widgets/world_loading.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'COVID-19 Tracker',
-      home: SafeArea(child: HomePage()),
+      home: FutureBuilder(
+        future: initApp(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting)
+            return WorldLoading();
+          return HomePage();
+        },
+      ),
     );
+  }
+
+  // To init needed things before entering the App
+  Future<void> initApp() async {
+    await Prefs.getInstance().initSharedPrefs();
   }
 }
