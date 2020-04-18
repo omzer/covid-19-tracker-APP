f2ea1ef14f0924a4e09f1ab4dcd8465c9330c98d
Omar Sabri <oz9.sa7@gmail.com>
2020-04-19 00:27:39 +0300

    Add webview to view the infection map in the App

--- lib/ui/pages/map_page.dart
+++ lib/ui/pages/map_page.dart
@@ -1,8 +1,33 @@
+import 'package:covidtracker/repo/api.dart';
+import 'package:covidtracker/ui/widgets/dark_background.dart';
+import 'package:covidtracker/ui/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('map page')));
+    return Scaffold(
+      body: DarkBackground(
+        child: _buildBody(),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        DarkAppBar(
+          title: 'Infection map',
+        ),
+        Expanded(
+          child: WebviewScaffold(
+            url: API.getInstance().getMapLink(),
+            hidden: false,
+            withZoom: true,
+          ),
+        ),
+      ],
+    );
   }
 }
