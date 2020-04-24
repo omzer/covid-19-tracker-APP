4817a30da76fed34426fe8ec04376917a4ceb68b
Omar Sabri <oz9.sa7@gmail.com>
2020-04-24 18:17:46 +0300

    Add cities page

--- lib/ui/pages/cities_page.dart
+++ lib/ui/pages/cities_page.dart
@@ -1,7 +1,9 @@
+import 'package:covidtracker/models/region_info.dart';
 import 'package:covidtracker/repo/api.dart';
 import 'package:covidtracker/ui/widgets/dark_background.dart';
 import 'package:covidtracker/ui/widgets/home_appbar.dart';
 import 'package:covidtracker/ui/widgets/world_loading.dart';
+import 'package:covidtracker/utils/assets_utils.dart';
 import 'package:flutter/material.dart';
 
 class CitiesPage extends StatelessWidget {
@@ -24,10 +26,35 @@ class CitiesPage extends StatelessWidget {
           builder: (context, snap) {
             if (snap.connectionState == ConnectionState.waiting)
               return WorldLoading();
-            return Text('done');
+            return Expanded(child: _buildListOfCities(snap.data));
           },
         )
       ],
+    );
+  }
+
+  Widget _buildListOfCities(List<RegionInfo> list) {
+    list.sort((a, b) => -(a.cases - b.cases));
+
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        String name = list[index].name;
+        String cases = list[index].cases.toString();
+        return Card(
+          elevation: 2,
+          margin: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage:
+                  AssetImage(AssetsUtils.getInstance().getJPGImagePath(name)),
+              radius: 30,
+            ),
+            title: Text(list[index].name),
+            subtitle: Text(cases),
+          ),
+        );
+      },
     );
   }
 }
