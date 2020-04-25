7a0e14ee05a3ad996bb2b43ac0f79e2f770649fc
Omar Sabri <oz9.sa7@gmail.com>
2020-04-25 18:49:56 +0300

    init about page

--- lib/ui/pages/about_page.dart
+++ lib/ui/pages/about_page.dart
@@ -1,8 +1,38 @@
+import 'package:covidtracker/lang/locale.dart';
+import 'package:covidtracker/ui/widgets/about_items/about_developer.dart';
+import 'package:covidtracker/ui/widgets/about_items/data_source.dart';
+import 'package:covidtracker/ui/widgets/dark_background.dart';
+import 'package:covidtracker/ui/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('about page')));
+    this.context = context;
+    return Scaffold(
+      body: DarkBackground(
+        child: _buildBody(),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        DarkAppBar(title: AppLocale.getString(context, 'about')),
+        Expanded(child: _buildList()),
+      ],
+    );
+  }
+
+  Widget _buildList() {
+    return ListView(
+      children: <Widget>[
+        DataSource(),
+        AboutDeveloper(),
+      ],
+    );
   }
 }
